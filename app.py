1ac9111ef2cb2e24b5cebc965f1b5dc0161244a1
pavankalyan <tpavankayanece98@gmail.com>
2026-06-15 04:49:12 +0000

    Revert "New features are Updated"
    This reverts commit 441d4ed12fc19e75afdecb3c9d36b38b024a9cdd.

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "GitHub Actions CI/CD pipeline with Docker is Working for flask App!"
+    return "GitHub Actions CI/CD with Docker is Working!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
--- check_docker.py
+++ check_docker.py
@@ -14,3 +14,4 @@ try:
 except Exception as e:
     print("Docker Not Found")
     print(e)
+
