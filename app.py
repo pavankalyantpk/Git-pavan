1e763f972ac699c82af3a457a67e6bfb9019b2c1
pavankalyan <tpavankayanece98@gmail.com>
2026-06-15 05:12:30 +0000

    Features Updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "GitHub Actions CI/CD with Docker is Working!"
+    return "GitHub Actions CI/CD pipeline with Docker is Working for Flask App !"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
